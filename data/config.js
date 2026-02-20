e5625a25f301b3d8be2b767825563e68a4e6e9de
CoolGuy-24 <mundraved@gmail.com>
2026-02-20 15:25:10 +0530

    Delete data/config.js

--- data/config.js
+++
@@ -1,4 +0,0 @@
-// ===== CC AUTHENTICATION KEY =====
-// This key is required when creating a Core Committee (CC) account.
-// Change this value to your own secret key.
-var CC_AUTH_KEY = "spons2026cc";
